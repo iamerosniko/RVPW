6333a64b13f3c85736a7a332532a352d2e353b78
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2017-09-03 14:53:04 +0800

    exam module complete

--- src/app/mod-workplace/exam/exam.module.ts
+++ src/app/mod-workplace/exam/exam.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ExamComponent } from './exam-component/exam-component.component';
+import { ExamFormComponent } from './exam-form-component/exam-form-component.component';
+import { ExamRouting } from './exam-routing';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,ExamRouting
   ],
-  declarations: []
+  declarations: [ExamComponent, ExamFormComponent]
 })
 export class ExamModule { }
--- src/app/mod-workplace/workplace.module.ts
+++ src/app/mod-workplace/workplace.module.ts
@@ -7,6 +7,7 @@ import { DisToolsModule } from './dis-tools/dis-tools.module';
 import { PoliciesModule } from './policies/policies.module';
 import { LeadersModule } from './leaders/leaders.module';
 import { TeamOverviewModule } from './team-overview/team-overview.module';
+import { ExamModule } from './exam/exam.module';
 @NgModule({
   imports: [
     CommonModule,
@@ -15,6 +16,7 @@ import { TeamOverviewModule } from './team-overview/team-overview.module';
     PoliciesModule, 
     LeadersModule,
     TeamOverviewModule,
+    ExamModule,
     WorkplaceRoutingModule
   ],
   declarations: [WorkplaceComponent]
